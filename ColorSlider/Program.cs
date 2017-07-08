ae5d427633947be8d8ba25bcf988d4e9f570891a
Beni Cohen <beni@mes-ltd.com>
2017-07-08 13:32:42 +0300

    Update Form name

--- ColorSlider/Program.cs
+++ ColorSlider/Program.cs
@@ -16,7 +16,7 @@ namespace ColorSlider
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
